f9a099cef228d9fe098e764ddfe0ad4c0a534c04
Leonardo Mendes <mendesleonardo475@gmail.com>
2023-03-29 17:38:30 -0300

    fix: conexão com o banco de dados

--- Back-End/src/repository/connection.js
+++ Back-End/src/repository/connection.js
@@ -2,10 +2,10 @@ import mysql from 'mysql2/promise'
 
 async function connect() {
   const connection = await mysql.createConnection({
-    host: '********',
-    user: '********',
-    password: '********',
-    database: '********'
+    host: 'bexnqaxu9hxqnn1pynsw-mysql.services.clever-cloud.com',
+    user: 'uho9bg78dgr9bpzz',
+    password: 'MAqhfWoXHV46ehIRbuXz',
+    database: 'bexnqaxu9hxqnn1pynsw'
   });
 
   return connection;
